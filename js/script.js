a3ac92ee91959dd847d6a6c1cadeb9b08623f515
Massidama <massidama@outlook.it>
2023-11-28 14:44:45 +0100

    add: click function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 // andiamo a dichiarare la costante container
 const container = document.querySelector(".container");
 
-for (let i = 1; i <= 100; i++) {
-        
-        // andiamo a creare il div
-        square =  `<div class="square"> ${i} </div>` ;
+const play = document.querySelector(".play");
 
-        // andiamo ad aggiungere il div al container 
-        container.innerHTML += square;
-}
+play.addEventListener ("click", 
+
+    function(){
+
+        for (let i = 1; i <= 100; i++) {
+        
+            // andiamo a creare il div
+            square =  `<div class="square"> ${i} </div>` ;
+    
+            // andiamo ad aggiungere il div al container 
+            container.innerHTML += square;
+    }
+
+    }
+)
